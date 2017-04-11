046524c5138405b6568155f0d5b2f97221fb7c2a
Matt Gabor <mattgabor@gmail.com>
2017-04-10 21:58:49 -0700

    added images & gulp routing

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -29054,6 +29054,31 @@ module.exports = About;
 
 var React = require('react');
 
+var Header = React.createClass({displayName: "Header",
+	render: function() {
+		return (
+        React.createElement("nav", {className: "navbar navbar-default"}, 
+          React.createElement("div", {className: "container-fluid"}, 
+              React.createElement("a", {href: "/", className: "navbar-brand"}, 
+                React.createElement("img", {src: "images/pluralsight-logo.png"})
+              ), 
+              React.createElement("ul", {className: "nav navbar-nav"}, 
+                React.createElement("li", null, React.createElement("a", {href: "/"}, "Home")), 
+                React.createElement("li", null, React.createElement("a", {href: "/#about"}, "About"))
+              )
+          )
+        )
+		);
+	}
+});
+
+module.exports = Header;
+
+},{"react":157}],160:[function(require,module,exports){
+"use strict";
+
+var React = require('react');
+
 var Home = React.createClass({displayName: "Home",
   render: function() {
     return (
@@ -29067,11 +29092,12 @@ var Home = React.createClass({displayName: "Home",
 
 module.exports = Home;
 
-},{"react":157}],160:[function(require,module,exports){
+},{"react":157}],161:[function(require,module,exports){
 $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
 var About = require('./components/about/aboutPage');
+var Header = require('./components/common/header');
 
 (function(win) {
   "use strict";
@@ -29086,6 +29112,7 @@ var About = require('./components/about/aboutPage');
 
       return (
         React.createElement("div", null, 
+          React.createElement(Header, null), 
           React.createElement(Child, null)
         )
       );
@@ -29100,4 +29127,4 @@ var About = require('./components/about/aboutPage');
   win.addEventListener('hashchange', render);
   render();
 })(window);
-},{"./components/about/aboutPage":158,"./components/homePage":159,"jquery":2,"react":157}]},{},[160]);
+},{"./components/about/aboutPage":158,"./components/common/header":159,"./components/homePage":160,"jquery":2,"react":157}]},{},[161]);
